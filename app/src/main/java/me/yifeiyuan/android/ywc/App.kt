e633e13f2da7537eca6b1fe713ec0c28e99e9fea
mingjue <zhening.czn@alibaba-inc.com>
2022-05-31 10:34:26 +0800

    update

---
+++ app/src/main/java/me/yifeiyuan/android/ywc/App.kt
@@ -0,0 +1,13 @@
+package me.yifeiyuan.android.ywc
+
+import android.app.Application
+
+/**
+ * Created by 程序亦非猿 on 2022/5/30.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
