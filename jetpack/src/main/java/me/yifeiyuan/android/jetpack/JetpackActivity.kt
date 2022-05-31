6590d25c806c8e566863caa30e717ad59172ffc3
mingjue <zhening.czn@alibaba-inc.com>
2022-05-31 14:55:44 +0800

    add jetpack recyclerview module

---
+++ jetpack/src/main/java/me/yifeiyuan/android/jetpack/JetpackActivity.kt
@@ -0,0 +1,11 @@
+package me.yifeiyuan.android.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JetpackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jetpack)
+    }
+}
